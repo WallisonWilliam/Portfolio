9151e8d5e0941e3b64d2cfb3f2f4d04a54b2c1b3
Wallison William <wallisonwilliamm@gmail.com>
2022-11-16 22:23:43 -0300

    Updating design and indentation

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,13 +1,13 @@
 window.sr = ScrollReveal({ reset: true });
-ScrollReveal({ distance: '15px' });
+ScrollReveal({ distance: '30px' });
 sr.reveal('.imagem-principal', { duration: 3000, origin: 'right' })
 sr.reveal('.descricao', { duration: 3000, origin: 'left' })
 sr.reveal('.linkedin', { duration: 3000, origin: 'left' })
 sr.reveal('.linha-horizontal', { duration: 3000, origin: 'left' })
 sr.reveal('.eu', { duration: 3000, origin: 'right' })
 sr.reveal('.projetos-itens', { duration: 3000, origin: 'bottom' })
-sr.reveal('.project', {duration : 3000, origin:'bottom'})
-sr.reveal('.repos-section', {duration : 3000, origin:'right'})
-sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
-sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
-sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+sr.reveal('.project', { duration: 3000, origin: 'bottom' })
+sr.reveal('.repos-section', { duration: 3000, origin: 'right' })
+sr.reveal('.github-link', { duration: 3000, origin: 'bottom' })
+sr.reveal('.conteiner-skills', { duration: 3000, origin: 'left' })
+sr.reveal('.conteiner-descricao', { duration: 3000, origin: 'right' })
